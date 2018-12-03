e5c6b43755888d545e992a903ae4aa24c4576dfa
Seth Watson <seth.watson1@gmail.com>
2018-12-03 13:31:29 -0700

    added 2 cards and flip actions connected

--- CardMatching/ViewController.swift
+++ CardMatching/ViewController.swift
@@ -15,6 +15,22 @@ class ViewController: UIViewController {
 
     }
     
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎄", on: sender)
+    }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎁", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.6762928367, green: 0.1193955019, blue: 0.04112524539, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 }
 
